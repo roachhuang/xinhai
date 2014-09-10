0c6f41d9005c77f735399296d721d58c7c4f07c5
Mark Huang <giraftw2002@gmail.com>
2014-09-10 14:39:06 +0800

    befroe branch

--- js/app.js
+++ js/app.js
@@ -1,57 +1,47 @@
 (function(){
-var app = angular.module("xinhai", []);
+	var app = angular.module('store', []);
 
-app.controller =("apartCtrl", function(){
-	this.doom = apart;
-	this.getDoomInfo = function(){
-		return this.doom;
-	};	
-});
+	app.controller("ApartController", function($scope){
+		$scope.doom = wow;	
+	});
+	app.controller('StoreController', function(){
+		this.products = wow;
+	});
+	var wow = {firstname:'mark', lastname: 'huang'};
+	/*
+var gems = [
+	{
+		name: 'Dope Diamond',
+		price: 82.95,
+		description: "Lorizzle ipsizzle dolor sit dang, we gonna chung that's the shizzle shut the shizzle up. Nullam tellivizzle bling bling, aliquet volutpizzle, suscipit break it down, gravida.",
+		specification: "Phasellizzle fo shizzle my nizzle crunk. Curabitur non velizzle the bizzle pede crackalackin facilisizzle. Bow wow wow rizzle nulla, iaculizzle fo shizzle, break.",
+		review: "Maecenas quis metus izzle fo shizzle my nizzle yo gangsta. Break yo neck, yall dope viverra fo. Curabitizzle sollicitudin boom shackalack quizzle purus.",
+		canPurchase: true,
+		soldOut: false,
+		images: [
+			{
+				full: 'img/diamond.png',
+				thumb: 'img/diamond-thumb.png'
+			}
+		],
+		reviews: [
+			{
+				stars: 5,
+				body: "I love this gem",
+				author: "joe@thomas.com"
+			},
+			{
+				stars: 3,
+				body: "I bought this for my wife",
+				author: "hubby@loveydovey.com"
+			}
+		]
+	}
+	];
 
-app.controller("photoCtrl", function(){
-    this.photos = pix; //product is a property of this controller
-  });
 
 
-app.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: '../home.html',
-        controller: 'PhoneListCtrl'
-      }).
-      when('/Photos', {
-        templateUrl: '../photos.html',
-        controller: 'galleryCtrl'
-      }).
-	 when('/Video', {
-        templateUrl: '../video.html',
-        controller: 'videoCtrl'
-      }).  
-	 when('/Map', {
-        templateUrl: '../map.html',
-        controller: 'mapCtrl'
-      }).  
-	 when('/Agreement', {
-        templateUrl: '../agreement.html',
-        controller: 'agreementCtrl'
-      }).
-	 when('/Contact', {
-        templateUrl: '../contact.html',
-        controller: 'contactCtrl'
-      }).  
-      otherwise({
-        redirectTo: '../home'
-      });
-  }]); 
-  
-  var apart = {
-	address:'2F, 90, Sec. 1, Xinhai Rd., Taipei',
-	rent: 20000,
-	lat: -34.397,
-	lng: 150.644,
-	size: 85	,	//sq meter
-	pix: [
+var apart =  [
      {
       name: 'Living Room',   
       description: 'Very Aqua-ish',
@@ -105,7 +95,40 @@ app.config(['$routeProvider',
 			  full: '../images/large/kitchen.jpg'
 			}
 		  ]
-	}		
-  ];
-  }
+	}	
+  ]; 
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: '../home.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/Photos', {
+        templateUrl: '../photos.html',
+        controller: 'galleryCtrl'
+      }).
+	 when('/Video', {
+        templateUrl: '../video.html',
+        controller: 'videoCtrl'
+      }).  
+	 when('/Map', {
+        templateUrl: '../map.html',
+        controller: 'mapCtrl'
+      }).  
+	 when('/Agreement', {
+        templateUrl: '../agreement.html',
+        controller: 'agreementCtrl'
+      }).
+	 when('/Contact', {
+        templateUrl: '../contact.html',
+        controller: 'contactCtrl'
+      }).  
+      otherwise({
+        redirectTo: '../home'
+      });
+  }]); 
+ */ 
+ 
 })();
