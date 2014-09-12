3fe91276eeb2644312f97341bef389b62bd870c2
Mark Huang <giraftw2002@gmail.com>
2014-09-12 19:38:16 +0800

    add route

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 (function(){	
-	var app = angular.module('myapp', []);
+	var app = angular.module('myapp', ['ngRoute']);
 
 	app.controller('ApartController', function(){
 		this.products= gems;
@@ -7,7 +7,7 @@
 			img.slidetoggle();
 		};
 	});
-
+	
 	app.controller('PanelController', function(){
 		this.tab = 1;
 		this.selectTab = function(setTab){
@@ -23,6 +23,30 @@
 			templateUrl: 'menu.html'
 		};
 	});
+	app.controller('mapController', function($scope){
+	});
+	app.controller('homeController', function($scope){
+	});
+	app.config(function($routeProvider){		
+		$routeProvider.
+		when("/",
+			{			
+				controller: 'homeController',
+				templateUrl: "./photos.html"
+			})
+		.when("/photos",
+			{			
+				controller: 'homeController',
+				templateUrl: "/photos.html"
+			})	
+		.when("/map", 
+			{
+				controller: "mapController",
+				templateUrl: "/map.html"
+			})
+			.otherwise({redirectTo: '/'});			
+	});
+	
 	var gems = [
 	{
 		name: 'Living Room',		
@@ -50,4 +74,40 @@
 	
 	];
 
+	// var marker = "";	// also global var
+	// Taipower MRT Ext 2
+	function initialize() {
+		var map = {}; 
+		//var myLatLng = new google.maps.LatLng(-34.397, 150.644);
+		var myLatLng = new google.maps.LatLng(25.029203,121.549028);
+		var mapOptions = {
+			zoom: 18,
+			center: myLatLng,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+
+		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+		// var myMarker='farm.png';
+		marker = new google.maps.Marker({
+			position: myLatLng,		
+			title: 'here',	
+			// icon: myMarker,
+			map: map
+		});
+			
+		//map.bindTo(marker);
+		//console.log(marker.getMap());
+		//map.addOverlay(marker);
+		google.maps.event.addDomListener(window, "resize", function() {
+				//var center = map.getCenter();
+				//google.maps.event.trigger(map, "resize");
+				map.setCenter(myLatLng);
+		});			
+	}; 
+
+	function processFromData(){
+		var msg = document.GetElementById('#msg').value;
+		var id = "+886987656201";
+		//window.location="whatsapp://send?abid=id&text=msg" );
+	};
 })();
