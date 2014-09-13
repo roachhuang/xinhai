b6157050d8135376419781cae2e66ced28a42d18
roach <mark.huang@ca-sec.com>
2014-09-13 21:48:01 +0800

    add mapcontroller

--- js/app.js
+++ js/app.js
@@ -38,12 +38,27 @@
 			{			
 				controller: 'homeController',
 				templateUrl: "./photos.html"
-			})	
+			})
+		.when("/video",
+			{			
+				controller: 'homeController',
+				templateUrl: "./video.html"
+			})			
 		.when("/map", 
 			{
 				controller: "mapController",
 				templateUrl: "./map.html"
 			})
+		.when("/agreement",
+			{			
+				controller: 'homeController',
+				templateUrl: "./agreement.html"
+			})
+		.when("/about",
+			{			
+				controller: 'homeController',
+				templateUrl: "./about.html"
+			})			
 			.otherwise({redirectTo: '/'});			
 	});
 	
@@ -76,34 +91,37 @@
 
 	// var marker = "";	// also global var
 	// Taipower MRT Ext 2
-	function initialize() {
-		var map = {}; 
-		//var myLatLng = new google.maps.LatLng(-34.397, 150.644);
-		var myLatLng = new google.maps.LatLng(25.029203,121.549028);
-		var mapOptions = {
-			zoom: 18,
-			center: myLatLng,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
+	app.controller('mapController', function(){
 
-		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-		// var myMarker='farm.png';
-		marker = new google.maps.Marker({
-			position: myLatLng,		
-			title: 'here',	
-			// icon: myMarker,
-			map: map
-		});
-			
-		//map.bindTo(marker);
-		//console.log(marker.getMap());
-		//map.addOverlay(marker);
-		google.maps.event.addDomListener(window, "resize", function() {
-				//var center = map.getCenter();
-				//google.maps.event.trigger(map, "resize");
-				map.setCenter(myLatLng);
-		});			
-	}; 
+		this.init = function(){
+			var map = {}; 
+			//var myLatLng = new google.maps.LatLng(-34.397, 150.644);
+			var myLatLng = new google.maps.LatLng(25.029203,121.549028);
+			var mapOptions = {
+				zoom: 18,
+				center: myLatLng,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+
+			map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+			// var myMarker='farm.png';
+			marker = new google.maps.Marker({
+				position: myLatLng,		
+				title: 'here',	
+				// icon: myMarker,
+				map: map
+			});
+				
+			//map.bindTo(marker);
+			//console.log(marker.getMap());
+			//map.addOverlay(marker);
+			google.maps.event.addDomListener(window, "resize", function() {
+					//var center = map.getCenter();
+					//google.maps.event.trigger(map, "resize");
+					map.setCenter(myLatLng);
+			});			
+		}; 
+	});	
 
 	function processFromData(){
 		var msg = document.GetElementById('#msg').value;
