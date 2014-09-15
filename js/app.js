2955d7ce19ed515124dae48bc55e1d855807a9bf
roach <mark.huang@ca-sec.com>
2014-09-15 22:09:29 +0800

    add $http for apartcontroller, change container-fluid to container, map controller

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 (function(){	
 	var app = angular.module('myapp', ['ngRoute']);
 
-	app.controller('ApartController', function(){
-		this.products= gems;
-		this.slidePix = function(img){
-			img.slidetoggle();
-		};
-	});
+	app.controller('ApartController', [ '$http', function($http){
+		var that = this;
+		$http.get('/xinhai/rooms.json').success(function(data){
+			that.products = data;
+		});		
+	}]);
 	
 	app.controller('PanelController', function(){
 		this.tab = 1;
@@ -23,10 +23,44 @@
 			templateUrl: '/xinhai/menu.html'
 		};
 	});
-	app.controller('mapController', function($scope){
-	});
+	
 	app.controller('homeController', function($scope){
 	});
+	// var marker = "";	// also global var
+	// Taipower MRT Ext 2
+	app.controller('mapController', function(){
+
+		this.init = function(){
+			var map = {}; 
+			var marker = {};
+			//var myLatLng = new google.maps.LatLng(-34.397, 150.644);
+			var myLatLng = new google.maps.LatLng(25.029203,121.549028);
+			var mapOptions = {
+				zoom: 18,
+				center: myLatLng,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+
+			map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+			// var myMarker='farm.png';
+			marker = new google.maps.Marker({
+				position: myLatLng,		
+				title: 'here',	
+				// icon: myMarker,
+				map: map
+			});
+				
+			//map.bindTo(marker);
+			//console.log(marker.getMap());
+			//map.addOverlay(marker);
+			google.maps.event.addDomListener(window, "resize", function() {
+					//var center = map.getCenter();
+					//google.maps.event.trigger(map, "resize");
+					map.setCenter(myLatLng);
+			});			
+		}; 
+	});	
+
 	app.config(function($routeProvider){		
 		$routeProvider.
 		when("/",
@@ -61,71 +95,5 @@
 			})			
 			.otherwise({redirectTo: '/'});			
 	});
-	
-	var gems = [
-	{
-		name: 'Living Room',		
-		description: "Lorizzle ipsizzle dolor sit dang, we gonna chung that's the shizzle shut the shizzle up. Nullam tellivizzle bling bling, aliquet volutpizzle, suscipit break it down, gravida.",
-		
-		images: [
-			{
-				full: './images/large/livingroom.jpg',
-				thumb: './images/small/livingroom.png'
-			}
-		],
-	
-	},
-	{
-		name: 'Dinning Room',		
-		description: "Maecenas quis metus izzle fo shizzle my nizzle yo gangsta. Break yo neck, yall dope viverra fo. Curabitizzle sollicitudin boom shackalack quizzle purus.",		
-		images: [
-			{
-				full: './images/large/dinningroom.jpg',
-				thumb: './images/small/dinningroom.jpg'
-			}
-		],
-		
-	},
-	
-	];
 
-	// var marker = "";	// also global var
-	// Taipower MRT Ext 2
-	app.controller('mapController', function(){
-
-		this.init = function(){
-			var map = {}; 
-			//var myLatLng = new google.maps.LatLng(-34.397, 150.644);
-			var myLatLng = new google.maps.LatLng(25.029203,121.549028);
-			var mapOptions = {
-				zoom: 18,
-				center: myLatLng,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			};
-
-			map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-			// var myMarker='farm.png';
-			marker = new google.maps.Marker({
-				position: myLatLng,		
-				title: 'here',	
-				// icon: myMarker,
-				map: map
-			});
-				
-			//map.bindTo(marker);
-			//console.log(marker.getMap());
-			//map.addOverlay(marker);
-			google.maps.event.addDomListener(window, "resize", function() {
-					//var center = map.getCenter();
-					//google.maps.event.trigger(map, "resize");
-					map.setCenter(myLatLng);
-			});			
-		}; 
-	});	
-
-	function processFromData(){
-		var msg = document.GetElementById('#msg').value;
-		var id = "+886987656201";
-		//window.location="whatsapp://send?abid=id&text=msg" );
-	};
 })();
