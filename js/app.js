065034f57cac1022c75f3a5b51df6c19a17fca07
roach <mark.huang@ca-sec.com>
2014-09-21 23:03:00 +0800

    add animation to ng-view

--- js/app.js
+++ js/app.js
@@ -132,8 +132,8 @@
 			})			
 			.otherwise({redirectTo: '/'});			
 	});
-
-	ngModule.animation('.expand', function() {
+	/*
+	app.animation('.expand', function() {
 		return {
 			enter : function(element, done) {
 				element.css({ 'left': -50, 'opacity': 0});
@@ -146,5 +146,36 @@
 			}
 		}
 	});	
+	*/
+
+	app.animation('.fadeIn', function() {
+		return {
+			enter: function(element, done) {
+		    	//run the animation here and call done when the animation is complete
+		    	jQuery(element).hide().slideDown('slow', done);		    	
+	        	return function(cancelled) {
+		        //this (optional) function will be called when the animation
+		        //completes or when the animation is cancelled (the cancelled
+		        //flag will be set to true if cancelled).
+				};
+			},
+			leave: function(element, done) {
+				jQuery(element).slideUp('slow', done);
+			},
+			move: function(element, done) { },
+
+		    //animation that can be triggered before the class is added
+		    beforeAddClass: function(element, className, done) { },
+
+		    //animation that can be triggered after the class is added
+		    addClass: function(element, className, done) { },
+
+		    //animation that can be triggered before the class is removed
+		    beforeRemoveClass: function(element, className, done) { },
+
+		    //animation that can be triggered after the class is removed
+		    removeClass: function(element, className, done) { }
+		};
+	});
 
 })();
